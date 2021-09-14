5776ce34920b53f7065f7f5e2f9f1d18b4d1e07c
Asante Cureton <asantercureton@gmail.com>
2021-09-13 21:05:56 -0400

    Updated javascript to write a README and included a Table of Contents with the help of my tutor.

---
+++ README2.md
@@ -0,0 +1,29 @@
+# This works
+## Table Of Contents
+[Instructions](#instructions)
+
+[Usage](#usage)
+
+[Contributing](#contributing)
+
+[Test](#test)
+
+[License](#license)
+
+[GitHub](#gitHub)
+## Installation
+install
+## Instructions
+follow instr
+## Usage
+easy
+## Contributing
+email me
+## Test
+email me
+## License
+![GitHub license](https://img.shields.io/badge/license-ISC-blue.svg)
+## Description
+details here
+## GitHub Username
+github.com
--- index.js
+++ index.js
@@ -1,177 +1,261 @@
 // ARRANGE
-const fs = require('fs');
 const inquirer = require('inquirer');
+const fs = require('fs');
 
 const outputREADME = (readMe) => {
-    fs.writeFile('README.md', readMe, (err) => {
+    fs.writeFile('README2.md', readMe, (err) => {
         if (err) console.log(err);
         console.log('Success');
     })
 };
 
 
-// ACTION (Pure Functions)
-function init() {
-    inquirer.prompt([
-        {
-            type: 'input',
-            message: 'What is the title of your project?',
-            name: 'title',
-            validate: your_title => {
-                if (your_title) {
-                    return true;
-                }   else {
-                    console.log('Provide a title for your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What was your motivation for developing your project?',
-            name: 'description',
-            validate: your_description => {
-                if (your_description) {
-                    return true;
-                }   else {
-                    console.log('Provide a description for your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'How do you install your project?',
-            name: 'installation',
-            validate: your_installation => {
-                if (your_installation) {
-                    return true;
-                }   else {
-                    console.log('Provide installation details for your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What are the instructions for your project?',
-            name: 'instructions',
-            validate: your_instructions => {
-                if (your_instructions) {
-                    return true;
-                }   else {
-                    console.log('Provide instructions for your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'How is your project designed to be used?',
-            name: 'usage',
-            validate: your_usage => {
-                if (your_usage) {
-                    return true;
-                }   else {
-                    console.log('Provide usage details for your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'How can someone contribute to your project?',
-            name: 'contributing',
-            validate: your_contributing => {
-                if (your_contributing) {
-                    return true;
-                }   else {
-                    console.log('Provide instructions for making contributions to your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'How can your project be tested?',
-            name: 'test',
-            validate: your_test => {
-                if (your_test) {
-                    return true;
-                }   else {
-                    console.log('Provide test details for your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'checkbox',
-            message: 'Which license is your project covered under?',
-            name: 'license',
-            choices: [
-                'BSD',
-                'MIT',
-                'GPL',
-                'ISC',
-            ],
-            validate: your_license => {
-                if (your_license) {
-                    return true;
-                }   else {
-                    console.log('Select a license for your project');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your GitHub username?',
-            name: 'gitHub',
-            validate: your_gitHub => {
-                if (your_gitHub) {
-                    return true;
-                }   else {
-                    console.log('Provide your GitHub username');
-                    return false;
-                }
-            }
-        },
-        {
-            type: 'input',
-            message: 'What is your email address?',
-            name: 'email',
-            validate: your_email => {
-                if (your_email) {
-                    return true;
-                }   else {
-                    console.log('Provide your email address');
-                    return false;
-                }
-            }
-        },
-    ]).then(({title, description, installation, instructions, usage, contributing, test, license, gitHub}) => {
-        // title, description, installation, instructions, usage, contributing, test, license, gitHub
-//         const titleTemplate = renderTitle(title);
-//         const descriptionTemplate = renderDescription(description);
-//         const installationTemplate = renderInstallation(installation);
-//         const instructionsTemplate = renderInstructions(instructions);
-//         const usageTemplate = renderUsage(usage);
-//         const contributingTemplate = renderContributing(contributing);
-//         const testTemplate = renderTest(test);
-//         const licenseTemplate = renderLicense(license);
-//         const gitHubTemplate = renderGitHub(gitHub);
-//         const baseTemplate = renderBaseTemplate(
-//             titleTemplate,
-//             descriptionTemplate,
-//             installationTemplate,
-//             instructionsTemplate,
-//             usageTemplate,
-//             contributingTemplate,
-//             testTemplate,
-//             licenseTemplate,
-//             gitHubTemplate,
-//         );
-//     })
-// };
-// WRITE FILE
-// CALL ON FUNCTION
+// ACTION
+const renderTitle = (title) => {
+    return `# ${title}`
+};
+
+const renderDescription = (description) => {
+    return `## Description
+${description}`
+};
+
+const renderTableOfContents = () => {
+    return `## Table Of Contents
+[Instructions](#instructions)
+
+[Usage](#usage)
+
+[Contributing](#contributing)
+
+[Test](#test)
+
+[License](#license)
+
+[GitHub](#gitHub)`
+};
+
+const renderInstallation = (installation) => {
+    return `## Installation
+${installation}`
+};
+
+const renderInstructions = (instructions) => {
+    return `## Instructions
+${instructions}`
+};
+
+const renderUsage = (usage) => {
+    return `## Usage
+${usage}`
+};
+
+const renderContributing = (contributing) => {
+    return `## Contributing
+${contributing}`
+};
+
+const renderTest = (test) => {
+    return `## Test
+${test}`
+};
+
+const renderLicense = (license) => {
+    return `## License
+![GitHub license](https://img.shields.io/badge/license-${license}-blue.svg)`
+};
+
+const renderGitHub = (gitHub) => {
+    return `## GitHub Username
+${gitHub}`
+};
+
+const renderBaseTemplate = (
+    titleTemplate,
+    licenseTemplate,
+    descriptionTemplate,
+    tableOfContentsTemplate,
+    installationTemplate,
+    instructionsTemplate,
+    usageTemplate,
+    contributingTemplate,
+    testTemplate,
+    gitHubTemplate,
+    ) => {
+    return `${titleTemplate}
+${descriptionTemplate}
+${tableOfContentsTemplate}
+${installationTemplate}
+${instructionsTemplate}
+${usageTemplate}
+${contributingTemplate}
+${testTemplate}
+${licenseTemplate}
+${gitHubTemplate}`
+};
+
+var questions = [{
+    type: 'input',
+    message: 'What is the title of your project?',
+    name: 'title',
+    validate: your_title => {
+        if (your_title) {
+            return true;
+        } else {
+            console.log('Provide a title for your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'What was your motivation for developing your project?',
+    name: 'description',
+    validate: your_description => {
+        if (your_description) {
+            return true;
+        } else {
+            console.log('Provide a description for your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'How do you install your project?',
+    name: 'installation',
+    validate: your_installation => {
+        if (your_installation) {
+            return true;
+        } else {
+            console.log('Provide installation details for your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'What are the instructions for your project?',
+    name: 'instructions',
+    validate: your_instructions => {
+        if (your_instructions) {
+            return true;
+        } else {
+            console.log('Provide instructions for your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'How is your project designed to be used?',
+    name: 'usage',
+    validate: your_usage => {
+        if (your_usage) {
+            return true;
+        } else {
+            console.log('Provide usage details for your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'How can someone contribute to your project?',
+    name: 'contributing',
+    validate: your_contributing => {
+        if (your_contributing) {
+            return true;
+        } else {
+            console.log('Provide instructions for making contributions to your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'How can your project be tested?',
+    name: 'test',
+    validate: your_test => {
+        if (your_test) {
+            return true;
+        } else {
+            console.log('Provide test details for your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'checkbox',
+    message: 'Which license is your project covered under?',
+    name: 'license',
+    choices: [
+        'BSD',
+        'MIT',
+        'GPL',
+        'ISC',
+    ],
+    validate: your_license => {
+        if (your_license) {
+            return true;
+        } else {
+            console.log('Select a license for your project');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'What is your GitHub username?',
+    name: 'gitHub',
+    validate: your_gitHub => {
+        if (your_gitHub) {
+            return true;
+        } else {
+            console.log('Provide your GitHub username');
+            return false;
+        }
+    }
+},
+{
+    type: 'input',
+    message: 'What is your email address?',
+    name: 'email',
+    validate: your_email => {
+        if (your_email) {
+            return true;
+        } else {
+            console.log('Provide your email address');
+            return false;
+        }
+    }
+},
+
+]
+inquirer.prompt(questions)
+.then(({ title, description, installation, instructions, usage, contributing, test, license, gitHub }) => {
+    // title, description, installation, instructions, usage, contributing, test, license, gitHub
+    const titleTemplate = renderTitle(title);
+    const descriptionTemplate = renderDescription(description);
+    const tableOfContentsTemplate = renderTableOfContents();
+    const installationTemplate = renderInstallation(installation);
+    const instructionsTemplate = renderInstructions(instructions);
+    const usageTemplate = renderUsage(usage);
+    const contributingTemplate = renderContributing(contributing);
+    const testTemplate = renderTest(test);
+    const licenseTemplate = renderLicense(license);
+    const gitHubTemplate = renderGitHub(gitHub);
+    const baseTemplate = renderBaseTemplate(
+        titleTemplate,
+        descriptionTemplate,
+        tableOfContentsTemplate,
+        installationTemplate,
+        instructionsTemplate,
+        usageTemplate,
+        contributingTemplate,
+        testTemplate,
+        licenseTemplate,
+        gitHubTemplate,
+    );
+    outputREADME(baseTemplate);
+});
